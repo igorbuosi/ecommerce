d43d3b06c78f2cbf3db76f2122d39c6e101289f8
Igor Buosi <igorbiancobuosi@hotmail.com>
2022-09-23 20:15:31 -0300

    Alterar senha

--- site.php
+++ site.php
@@ -11,6 +11,9 @@ use Hcode\Model\OrderStatus;
 
 $app->get('/', function() { //definindo a rota padrao / -> sem chamar nada  
 
+	/*var_dump(password_verify('123456','$2y$12$/DGVAB.lbNQTGmM16tgCV.OtKOsjOHrUiJedrdADVLvLE2JmLX1Je'));
+	exit;*/
+
 	$products = Product :: listAll();
 
 	$page = new Page(); // instanciou o construct
@@ -532,6 +535,80 @@ $app->get("/profile/orders/:idorder", function($idorder){
 	]);
 });
 
+
+$app->get("/profile/change-password", function(){
+	User::verifyLogin(false);
+
+	$page = new Page();
+	$page->setTpl("profile-change-password",[
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+
+
+});
+
+$app->post("/profile/change-password", function(){
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+
+		User::setError("Digite a senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+
+		User::setError("Digite a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+
+		User::setError("Confirme a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+
+		User::setError("A sua nova senha deve ser diferente da atual.");
+		header("Location: /profile/change-password");
+		exit;		
+
+	}
+
+	$user = User::getFromSession();
+
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setError("A senha está inválida.");
+		header("Location: /profile/change-password");
+		exit;			
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha alterada com sucesso.");
+
+	header("Location: /profile/change-password");
+	exit;
+
+
+
+
+
+
+	
+});
 
 
 ?>
