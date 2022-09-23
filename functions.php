ec700a1e9fbccccd93ea2676e2f268fa3080daa0
Igor Buosi <igorbiancobuosi@hotmail.com>
2022-09-23 19:31:36 -0300

    Meus Pedidos e Resumo carrinho

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 Use Hcode\Model\User;
+use Hcode\Model\Cart;
     function formatPrice($vlprice){
 
         if (!$vlprice > 0) $vlprice = 0;
@@ -20,5 +21,19 @@ Use Hcode\Model\User;
 
         return $user->getdesperson();
         
+    }
+
+    function getCartNrQtd(){
+        $cart = Cart::getFromSession();
+        $totals= $cart->getProductsTotals();
+        return formatPrice($totals['nrqtd']);
+    }
+
+    function getCartVlSubTotal(){
+        $cart = Cart::getFromSession();
+        $totals= $cart->getProductsTotals();
+
+        return formatPrice($totals['vlprice']);
+
     }
 ?>
